38e02b268c952314e55c4455345557a5aef569c5
Jan Warchoł <jan.warchol@gmail.com>
2018-03-03 16:29:13 +0100

    Move modifier keys around

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -131,18 +131,18 @@ enum { QWERTY, NUMPAD, FUNCTION }; // layers
 const Key keymaps[][ROWS][COLS] PROGMEM = {
 
   [QWERTY] = KEYMAP_STACKED
-  (___,             Key_1,         Key_2,       Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick,    Key_Q,         Key_W,       Key_E, Key_R, Key_T, Key_Tab,
-   Key_LeftShift,   Key_A,         Key_S,       Key_D, Key_F, Key_G,
-   ___,             Key_Z,         Key_X,       Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+  (Key_Backtick,  Key_1,           Key_2,       Key_3, Key_4, Key_5, Key_LEDEffectNext,
+   Key_Tab,       Key_Q,           Key_W,       Key_E, Key_R, Key_T, Key_Tab,
+   Key_LeftShift, Key_A,           Key_S,       Key_D, Key_F, Key_G,
+   ___,           Key_Z,           Key_X,       Key_C, Key_V, Key_B, Key_Backspace,
+   Key_LeftAlt,   Key_LeftControl, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),   Key_6,       Key_7,        Key_8,     Key_9,      Key_0,         LockLayer(NUMPAD),
-   Key_Enter,      Key_Y,       Key_U,        Key_I,     Key_O,      Key_P,         Key_Equals,
-   /*nokey*/       Key_H,       Key_J,        Key_K,     Key_L,      Key_Semicolon, Key_RightShift,
-   Key_RightAlt,   Key_N,       Key_M,        Key_Comma, Key_Period, Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
+   M(MACRO_ANY),   Key_6,            Key_7,        Key_8,     Key_9,      Key_0,         LockLayer(NUMPAD),
+   Key_Enter,      Key_Y,            Key_U,        Key_I,     Key_O,      Key_P,         Key_Equals,
+   /*nokey*/       Key_H,            Key_J,        Key_K,     Key_L,      Key_Semicolon, Key_RightShift,
+   Key_Backspace,  Key_N,            Key_M,        Key_Comma, Key_Period, Key_Slash,     Key_Minus,
+   Key_RightShift, Key_RightControl, Key_Spacebar, Key_RightAlt,
    ShiftToLayer(FUNCTION)),
 
 
@@ -165,14 +165,14 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
   (___,      Key_F1,          Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
    Key_Tab,  ___,             Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
    Key_Home, Key_mouseL,      Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen, Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
+   Key_End,  Key_PrintScreen, Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_Delete,
    ___,      Key_Delete,      ___,         ___,
    ___,
 
    Consumer_ScanPreviousTrack, Key_F6,                   Key_F7,                   Key_F8,                Key_F9,          Key_F10,          Key_F11,
    Consumer_PlaySlashPause,    Consumer_ScanNextTrack,   Key_LeftCurlyBracket,     Key_RightCurlyBracket, Key_LeftBracket, Key_RightBracket, Key_F12,
    /*nokey*/                   Consumer_VolumeIncrement, Key_LeftArrow,            Key_DownArrow,         Key_UpArrow,     Key_RightArrow,   Key_Quote,
-   Key_PcApplication,          Consumer_VolumeDecrement, Consumer_Mute,            Key_PageDown,          Key_PageUp,      Key_Backslash,    Key_Pipe,
+   Key_Delete,                 Consumer_VolumeDecrement, Consumer_Mute,            Key_PageDown,          Key_PageUp,      Key_Backslash,    Key_Pipe,
    ___,                        ___,                      Key_Enter,                ___,
    ___)
 
