b77338f8218838d89d719f093276a857c8f5c93c
Jan Warchoł <jan.warchol@gmail.com>
2018-03-03 16:37:11 +0100

    Fixes for sticky function key

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -164,16 +164,16 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
   [FUNCTION] =  KEYMAP_STACKED
   (___,      Key_F1,          Key_F2,      Key_F3,     Key_F4,        Key_F5,           XXX,
    Key_Tab,  ___,             Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
-   Key_Home, Key_mouseL,      Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen, Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_Delete,
-   ___,      Key_Delete,      ___,         ___,
+   ___,      Key_mouseL,      Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
+   ___,      Key_PrintScreen, Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_Delete,
+   ___,      ___,             ___,         ___,
    ___,
 
    Consumer_ScanPreviousTrack, Key_F6,                   Key_F7,                   Key_F8,                Key_F9,          Key_F10,          Key_F11,
    Consumer_PlaySlashPause,    Consumer_ScanNextTrack,   Key_LeftCurlyBracket,     Key_RightCurlyBracket, Key_LeftBracket, Key_RightBracket, Key_F12,
    /*nokey*/                   Consumer_VolumeIncrement, Key_LeftArrow,            Key_DownArrow,         Key_UpArrow,     Key_RightArrow,   Key_Quote,
    Key_Delete,                 Consumer_VolumeDecrement, Key_Home,                 Key_PageDown,          Key_PageUp,      Key_End,          Key_Pipe,
-   ___,                        ___,                      Key_Enter,                ___,
+   ___,                        ___,                      ___,                      ___,
    ___)
 
 };
