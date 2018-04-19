8345b6cae3f878bb171c02260b0e8be412af682a
Jan Warchoł <jan.warchol@gmail.com>
2018-04-19 17:37:49 +0200

    Add original qwerty layout

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -123,7 +123,7 @@ enum { MACRO_VERSION_INFO,
   *
   */
 
-enum { JANEK, NUMPAD, FUNCTION }; // layers
+enum { JANEK, QWERTY, NUMPAD, FUNCTION }; // layers
 
 /* This comment temporarily turns off astyle's indent enforcement
  *   so we can make the keymaps actually resemble the physical key layout better
@@ -144,7 +144,22 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_Enter,      Key_B,            Key_U,        Key_I,     Key_O,      Key_J,         Key_Minus,
    /*nokey*/       Key_Y,            Key_N,        Key_R,     Key_L,      Key_H, Key_RightShift,
    Key_Backspace,  Key_P,            Key_M,        Key_K,     Key_Period, Key_Slash,     Key_Equals,
-   LockLayer(FUNCTION), Key_RightControl, Key_Spacebar, Key_RightAlt,
+   LockLayer(QWERTY), Key_RightControl, Key_Spacebar, Key_RightAlt,
+   ShiftToLayer(FUNCTION)),
+
+  [QWERTY] = KEYMAP_STACKED
+  (___,             Key_1,         Key_2,       Key_3, Key_4, Key_5, Key_LEDEffectNext,
+   Key_Backtick,    Key_Q,         Key_W,       Key_E, Key_R, Key_T, Key_Tab,
+   Key_PageUp,      Key_A,         Key_S,       Key_D, Key_F, Key_G,
+   Key_PageDown,    Key_Z,         Key_X,       Key_C, Key_V, Key_B, Key_Escape,
+   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+   ShiftToLayer(FUNCTION),
+
+   M(MACRO_ANY),   Key_6,       Key_7,        Key_8,     Key_9,      Key_0,         LockLayer(NUMPAD),
+   Key_Enter,      Key_Y,       Key_U,        Key_I,     Key_O,      Key_P,         Key_Equals,
+   /*nokey*/       Key_H,       Key_J,        Key_K,     Key_L,      Key_Semicolon, Key_Quote,
+   Key_RightAlt,   Key_N,       Key_M,        Key_Comma, Key_Period, Key_Slash,     Key_Minus,
+   ___, Key_LeftAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
 
