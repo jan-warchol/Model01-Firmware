509b3432cb77e7f041d4e71eb6ceb4cc60d67f1b
Jan Warchoł <jan.warchol@gmail.com>
2018-04-20 09:20:16 +0200

    Full support for factory layout
    Include original function layer.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -126,7 +126,7 @@ enum { MACRO_VERSION_INFO,
   *
   */
 
-enum { JANEK, QWERTY, NUMPAD, FUNCTION }; // layers
+enum { JANEK, DFLT_QWER, DFLT_FN, NUMPAD, FUNCTION }; // layers
 
 /* This comment temporarily turns off astyle's indent enforcement
  *   so we can make the keymaps actually resemble the physical key layout better
@@ -147,23 +147,38 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_Enter,         Key_B,            Key_U,        Key_I, Key_O,      Key_J,     Key_Minus,
    /*nokey*/          Key_Y,            Key_N,        Key_R, Key_L,      Key_H,     OSM(RightShift),
    Key_Backspace,     Key_P,            Key_M,        Key_K, Key_Period, Key_Slash, Key_Equals,
-   LockLayer(QWERTY), Key_RightControl, Key_Spacebar, OSM(RightAlt),
+   ___, Key_RightControl, Key_Spacebar, OSM(RightAlt),
    OSL(FUNCTION)),
 
-  [QWERTY] = KEYMAP_STACKED
+  [DFLT_QWER] = KEYMAP_STACKED
   (___,             Key_1,         Key_2,       Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Backtick,    Key_Q,         Key_W,       Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,      Key_A,         Key_S,       Key_D, Key_F, Key_G, /*nokey*/
    Key_PageDown,    Key_Z,         Key_X,       Key_C, Key_V, Key_B, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   ShiftToLayer(FUNCTION),
+   ShiftToLayer(DFLT_FN),
 
    M(MACRO_ANY), Key_6,       Key_7,        Key_8,     Key_9,      Key_0,         LockLayer(NUMPAD),
    Key_Enter,    Key_Y,       Key_U,        Key_I,     Key_O,      Key_P,         Key_Equals,
    /*nokey*/     Key_H,       Key_J,        Key_K,     Key_L,      Key_Semicolon, Key_Quote,
    Key_RightAlt, Key_N,       Key_M,        Key_Comma, Key_Period, Key_Slash,     Key_Minus,
    ___,          Key_LeftAlt, Key_Spacebar, Key_RightControl,
-   ShiftToLayer(FUNCTION)),
+   ShiftToLayer(DFLT_FN)),
+
+  [DFLT_FN] =  KEYMAP_STACKED
+  (___,      Key_F1,          Key_F2,      Key_F3,     Key_F4,        Key_F5,           LockLayer(DFLT_QWER),
+   Key_Tab,  ___,             Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
+   Key_Home, Key_mouseL,      Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
+   Key_End,  Key_PrintScreen, Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
+   ___,      Key_Delete,      ___,         ___,
+   ___,
+
+   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
+   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
+   /*nokey*/                   Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
+   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
+   ___,                        ___,                    Key_Enter,                ___,
+   ___),
 
 
   [NUMPAD] =  KEYMAP_STACKED
@@ -182,11 +197,11 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    ___),
 
   [FUNCTION] =  KEYMAP_STACKED
-  (___, Key_F1,     Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_F12,
-   ___, ___,        Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, LSHIFT(Key_Tab),
-   ___, Key_mouseL, Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW, /*nokey*/
-   ___, ___,        Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  ___,
-   ___, ___,        ___,         ___,
+  (Key_F12, Key_F1,     Key_F2,      Key_F3,     Key_F4,        Key_F5,           LockLayer(DFLT_QWER),
+   ___,     ___,        Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, LSHIFT(Key_Tab),
+   ___,     Key_mouseL, Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,  /*nokey*/
+   ___,     ___,        Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  ___,
+   ___,     ___,        ___,         ___,
    ___,
 
    Key_F11,  Key_F6,                   Key_F7,               Key_F8,                Key_F9,          Key_F10,          ___,
