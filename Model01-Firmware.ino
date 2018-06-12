67f3050a2a62c2d686279e0cfdd89189321623e9
Ania Abaloszewa <a.abalosheva@gmail.com>
2018-06-12 15:40:14 +0200

    Add missing RightShift to QWERTY

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -162,7 +162,7 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_Enter,    Key_Y,       Key_U,        Key_I,     Key_O,      Key_P,         Key_Equals,
    /*nokey*/     Key_H,       Key_J,        Key_K,     Key_L,      Key_Semicolon, Key_Quote,
    Key_RightAlt, Key_N,       Key_M,        Key_Comma, Key_Period, Key_Slash,     Key_Minus,
-   ___,          Key_LeftAlt, Key_Spacebar, Key_RightControl,
+   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(DFLT_FN)),
 
   [DFLT_FN] =  KEYMAP_STACKED
